f2c5ca59e264b194d3c9475f5ff642038e6eae9e
kflash <kflash123@gmail.com>
2016-07-06 07:39:24 +0800

    v.0.7.1

--- config/rollup.config.prod.js
+++ config/rollup.config.prod.js
@@ -6,7 +6,7 @@ import replace from 'rollup-plugin-replace';
 const pkg = JSON.parse( readFileSync( 'package.json', 'utf-8' ) );
 
 export default {
-  entry: `./build/src/${pkg.name}.js`,
+  entry: `./build/src/index.js`,
   plugins: [
       uglify({
         warnings: false,
