5e6a1b6d62767a59f21f81430af742ac85a0285a
KFlash <kflash123@gmail.com>
2016-07-06 00:31:39 +0800

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,7 @@ npm install -g npm@3.10.4
 
 # Install Chromium
 
-./scripts/ci/install_chromium.sh
+./scripts/install_chromium.sh
 
 # node tools/chromedriverpatch.js
 $(npm bin)/webdriver-manager update
