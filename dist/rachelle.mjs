770b3c8624596f9bbcaaa8caf376417b481de198
kflash <kflash123@gmail.com>
2016-07-07 19:36:04 +0800

    added environment variable demo

--- dist/rachelle.mjs
+++ dist/rachelle.mjs
@@ -1,9 +1,21 @@
 /*
 * rachelle.js
-* @version 0.7.7
+* @version 0.8.0
 * @copyright (c) 2016 - KFlash
 * @license MIT <undefined/blob/master/LICENSE>
 */
+/*
+ * This is the main entry point for your package.
+ *
+ * You can import other modules here, including external packages. When
+ * bundling using rollup you can mark those modules as external and have them
+ * excluded or, if they have a jsnext:main entry in their package.json (like
+ * this package does), let rollup bundle them into your dist file.
+ */
+// Environment variable example. This will be removed in the productionbuild
+if ("development" !== 'production') {
+    console.log('production build');
+}
 var index = {
     // 'a' should not be covered
     a: function () {
--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,18 @@
  * excluded or, if they have a jsnext:main entry in their package.json (like
  * this package does), let rollup bundle them into your dist file.
  */
+
+// Environment variable example. This will be removed in the productionbuild
+if (process.env.NODE_ENV !== 'production') {
+    console.log('production build');
+}
+
+// export default
 export default {
-   // 'a' should not be covered
-  a: (): boolean => {
-    const uncovered: boolean = true;
-    return uncovered;
-  },
-  b: (): boolean => true
+    // 'a' should not be covered
+    a: (): boolean => {
+        const uncovered: boolean = true;
+        return uncovered;
+    },
+    b: (): boolean => true
 };
